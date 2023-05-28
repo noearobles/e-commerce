f1fdd8be30883e708e0f66972bf35ca0c413b637
noearobles <roblesnoe1030@gmail.com>
2023-05-27 21:49:46 -0500

    modified html

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,19 +1,21 @@
-const placeForResults = document.querySelectorAll("contactResults")
-let fname = document.getElementById("fname");
-let address = document.getElementById("address");
-let email = document.getElementById("email");
-let phone = document.getElementById("phone");;
-let message = document.getElementById("message");
+// const placeForResults = document.querySelectorAll("results")
+// let fname = document.getElementById("fname");
+// let address = document.getElementById("address");
+// let email = document.getElementById("email");
+// let phone = document.getElementById("phone");;
+// let message = document.getElementById("message");
 
-contactForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+// contactForm.addEventListener('submit', (e) => {
+//     e.preventDefault();
 
-    let formData = {
-        fname: fname.value,
-        address: address.value,
-        email: email.value,
-        phone: phone.value,
-        message: message.value
-    }
-    console.log(formData)
-})
+//     let formData = {
+//         fname: fname.value,
+//         address: address.value,
+//         email: email.value,
+//         phone: phone.value,
+//         message: message.value
+//     }
+//     console.log(formData)
+
+
+// })
